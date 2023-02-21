2621fd5b86ce661a17312519fcb328fbbfbc8fa8
Milly <zmillyws@gmail.com>
2023-02-21 18:55:35 -0300

    ajeitado pastas

---
+++ lib/screens/loginofc.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginScreenTwo extends StatefulWidget {
+  const LoginScreenTwo({super.key});
+
+  @override
+  State<LoginScreenTwo> createState() => _LoginScreenTwoState();
+}
+
+class _LoginScreenTwoState extends State<LoginScreenTwo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
